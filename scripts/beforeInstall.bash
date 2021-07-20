70d47dca8ff0a9c887202d551afca53553006706
devwithpug <zmfjscl789@gmail.com>
2021-07-20 23:15:01 +0900

    for CodeDeploy

--- scripts/beforeInstall.bash
+++ scripts/beforeInstall.bash
@@ -8,4 +8,5 @@ docker stop cicd-test
 docker rm cicd-test
 
 if [[ "$(docker images -q devwithpug/ci-cd-with-github-actions 2> /dev/null)" != "" ]]; then
-  docker rmi -f $(docker images --format '{{.Repository}}:{{.Tag}}' --filter=reference='devwithpug/ci-cd-with-github-actions')
+  docker rmi -f $(docker images --format '{{.Repository}}:{{.Tag}}' --filter=reference='devwithpug/ci-cd-with-github-actions:latest')
+fi
