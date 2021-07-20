a9e43ad4e83bf8d352daafe049c12d9a460e0b5a
devwithpug <zmfjscl789@gmail.com>
2021-07-20 16:34:59 +0900

    init

---
+++ src/main/java/study/cicd/cicdwithgithubactions/CiCdWithGithubActionsApplication.java
@@ -0,0 +1,13 @@
+package study.cicd.cicdwithgithubactions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CiCdWithGithubActionsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CiCdWithGithubActionsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/study/cicd/cicdwithgithubactions/CiCdWithGithubActionsApplicationTests.java
@@ -0,0 +1,13 @@
+package study.cicd.cicdwithgithubactions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CiCdWithGithubActionsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
